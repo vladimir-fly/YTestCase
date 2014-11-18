efba3faf0d171fc6d7e28a7529bbd400c11a2f70
Vladimir Fly <vladimir-fly.com@ya.ru>
2014-11-18 21:53:28 +0300

    some changes

--- presentationViewer.js
+++ presentationViewer.js
@@ -1,1 +1,19 @@
-fdas
+(function ($, window, document) {
+    var pos = 0;
+    
+    for(var i=0; i < $(".slides .slide").length; i++) {
+        var val = 'li' + i;
+        $(".slider-body").append("<input type='radio' name='li' class=" + val + " id=" + i + ">");
+        $("." + val).click(function() { 
+                                        document.location.href = "#" + this.className;
+                                        pos = this.id;
+                                        console.log(pos);
+                                        });    
+    };
+    
+    $(".slider").click(function() {
+                                    pos = pos < $(".slides .slide").length - 1 ? ++pos : 0;
+                                    document.location.href = "#li" + pos;
+                                    $(".li" + pos).attr("checked", true);
+                                    });
+})(jQuery, window, document);
